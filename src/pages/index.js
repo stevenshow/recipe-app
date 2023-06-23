b27cf06215f0344c67fdd3401d90331a84ed98cf
stevenshow <steven.schoebinger@gmail.com>
2023-06-22 23:09:35 -0600

    remove unneeded classes

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export async function getStaticProps() {
 export default function Home({recipes}) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12 sm:px-24 bg-beige">
-      <div className="z-10 w-full max-w-5xl items-center justify-between text-md">
+      <div className="z-10 w-full max-w-5xl items-center">
       <h1 className="text-4xl text-primary-coral font-bold mb-12 text-left font-lobster">Kayla&apos;s Recipes</h1>
         {recipes.map((recipe, index) => {
           return <Recipe recipe={recipe} key={index} />
