9d195977f1978be43f0203f283e8e535e8f61f9f
stevenshow <steven.schoebinger@gmail.com>
2023-07-01 22:18:03 -0600

    add search functionality

---
+++ src/components/RecipeGroup/Search.jsx
@@ -0,0 +1,13 @@
+export default function Search({ type, value, onChange, placeholder, className }) {
+	return (
+		<div className={className}>
+			<input
+				type={type}
+				name="search"
+				className="block w-full rounded-md border-0 py-1.5 pl-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+				placeholder={placeholder}
+				onChange={onChange}
+			/>
+		</div>
+	);
+}
--- src/components/RecipeGroup/index.jsx
+++ src/components/RecipeGroup/index.jsx
@@ -1,41 +1,67 @@
+import { useState } from 'react';
+
 import Link from 'next/link';
 
 import getTags from '../Recipe/util/getTags';
+import Search from './Search';
 
 export default function RecipeList({ recipes }) {
+	const [searchTerm, setSearchTerm] = useState('');
+
+	const handleSearch = (event) => {
+		setSearchTerm(event.target.value);
+	};
+
+	const filteredRecipes = recipes.filter((recipe) => {
+		const tags = getTags(recipe);
+		const lowerCaseSearchTerm = searchTerm.toLowerCase();
+
+		return (
+			recipe.recipeName.toLowerCase().includes(lowerCaseSearchTerm) ||
+			tags.some((tag) => tag.toLowerCase().includes(lowerCaseSearchTerm))
+		);
+	});
+
 	return (
-		<div className="flex flex-wrap">
-			{recipes.map((recipe, index) => {
-				const tags = getTags(recipe);
-				tags.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
-				return (
-					<div className="w-full p-3 sm:w-1/2 lg:w-1/3" key={index}>
-						<Link
-							href={`/recipe/${recipe.id}`}
-							className={`flex h-full flex-col bg-herbGreen/40 text-center shadow hover:shadow-md`}
-						>
-							<div className="flex flex-grow flex-col justify-between p-8">
-								<h3 className="mt-6 font-georgia text-xl font-medium text-gray-700">
-									{recipe.recipeName}
-								</h3>
-								<div className="mt-1 text-sm italic text-gray-500">{recipe.description}</div>
-								<div className="mt-3 flex flex-wrap justify-center gap-2">
-									{tags.map((tag, index) => {
-										return (
-											<span
-												className="inline-flex items-center rounded-full bg-green-100 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/30"
-												key={index}
-											>
-												{tag}
-											</span>
-										);
-									})}
+		<div>
+			<Search
+				type="text"
+				value={searchTerm}
+				onChange={handleSearch}
+				placeholder="Search by title or tag"
+				className="m-auto w-5/6 max-w-[400px] px-3 pb-6"
+			/>
+			<div className="flex flex-wrap">
+				{filteredRecipes.map((recipe, index) => {
+					const tags = getTags(recipe);
+					tags.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+					return (
+						<div className="w-full p-3 sm:w-1/2 lg:w-1/3" key={index}>
+							<Link href={`/recipe/${recipe.id}`}>
+								<div className="flex h-full flex-col bg-herbGreen/40 text-center shadow hover:shadow-md">
+									<div className="flex flex-grow flex-col justify-between p-8">
+										<h3 className="mt-6 font-georgia text-xl font-medium text-gray-700">
+											{recipe.recipeName}
+										</h3>
+										<div className="mt-1 text-sm italic text-gray-500">{recipe.description}</div>
+										<div className="mt-3 flex flex-wrap justify-center gap-2">
+											{tags.map((tag, index) => (
+												<span
+													className="inline-flex items-center rounded-full bg-green-100 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/30"
+													key={index}
+												>
+													{tag}
+												</span>
+											))}
+										</div>
+									</div>
 								</div>
-							</div>
-						</Link>
-					</div>
-				);
-			})}
+							</Link>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
