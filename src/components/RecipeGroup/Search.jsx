90fa77da500179e56b30ada5b279b669c8b8c455
stevenshow <steven.schoebinger@gmail.com>
2023-07-01 22:42:33 -0600

    up search bar text size

--- src/components/RecipeGroup/Search.jsx
+++ src/components/RecipeGroup/Search.jsx
@@ -4,7 +4,7 @@ export default function Search({ type, value, onChange, placeholder, className }
 			<input
 				type={type}
 				name="search"
-				className="block w-full rounded-md border-0 py-1.5 pl-3 text-gray-900 shadow-sm ring-2 ring-inset ring-accent-herbGreen placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+				className="sm:text-md block w-full rounded-md border-0 py-1.5 pl-3 text-gray-900 shadow-sm ring-2 ring-inset ring-accent-herbGreen placeholder:text-gray-400 focus:ring-0 sm:leading-6"
 				placeholder={placeholder}
 				onChange={onChange}
 				value={value}
