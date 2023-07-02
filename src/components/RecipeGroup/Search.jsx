47c89f900a7f1033b8a9e12b7de6ca6b799168ff
stevenshow <steven.schoebinger@gmail.com>
2023-07-01 22:21:41 -0600

    add value prop

--- src/components/RecipeGroup/Search.jsx
+++ src/components/RecipeGroup/Search.jsx
@@ -7,6 +7,7 @@ export default function Search({ type, value, onChange, placeholder, className }
 				className="block w-full rounded-md border-0 py-1.5 pl-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 				placeholder={placeholder}
 				onChange={onChange}
+				value={value}
 			/>
 		</div>
 	);
