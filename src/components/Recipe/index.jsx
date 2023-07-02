c4e9ad7ec22dbf27f9acc3194b72fa966bca2c79
stevenshow <steven.schoebinger@gmail.com>
2023-07-01 22:42:19 -0600

    fix recipe details page padding

--- src/components/Recipe/index.jsx
+++ src/components/Recipe/index.jsx
@@ -15,7 +15,7 @@ export default function Recipe({ recipe }) {
 	const apiUrl = 'https://cms.devsteve.net';
 
 	return (
-		<div className="flex flex-col gap-4 text-body-gray">
+		<div className="mt-8 flex flex-col gap-4 text-body-gray">
 			<Title title={recipe?.recipeName} />
 			{imagePath && (
 				<Image alt="recipe image" src={`${apiUrl}${imagePath}`} width={400} height={500} />
