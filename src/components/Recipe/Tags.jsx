98ad8bd418f90879088828f2e6e115a67b162107
stevenshow <steven.schoebinger@gmail.com>
2023-07-02 21:35:14 -0600

    fix mobile view

--- src/components/Recipe/Tags.jsx
+++ src/components/Recipe/Tags.jsx
@@ -2,7 +2,7 @@ import Tag from './Tag';
 
 export default function Tags({ tags }) {
 	return (
-		<div className="mt-4 flex gap-4">
+		<div className="mt-4 flex flex-wrap gap-4">
 			{tags.map((tag, index) => (
 				<Tag tag={tag} key={index} />
 			))}
